04514873f66a639a36e502c62b8291a04e1feb18
ColdCastlee <46883691+ColdCastlee@users.noreply.github.com>
2026-04-07 15:58:59 +0200

    Update urls.py

--- backend-django/eventhub/eventhub/urls.py
+++ backend-django/eventhub/eventhub/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from api.views import (
@@ -15,13 +16,15 @@ router.register(r"events", EventViewSet)
 router.register(r"participants", ParticipantViewSet)
 router.register(r"registrations", RegistrationViewSet)
 
+def home(request):
+    return HttpResponse("EventHub backend is running")
+
 urlpatterns = [
+    path("", home),
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
-
     path("api/register/", RegisterUserView.as_view()),
     path("api/me/", CurrentUserView.as_view()),
-
     path("api/token/", TokenObtainPairView.as_view()),
     path("api/token/refresh/", TokenRefreshView.as_view()),
 ]
